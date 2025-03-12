f510297f38492ef5e391da6317705a9e4985edb5
Wally Cruz <wallynhoogolfinho@gmail.com>
2025-03-12 12:39:41 -0300

    atualizando

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,11 @@
 import entities.Cliente;
 import entities.Esteticista;
 import entities.Procedimento;
-import orders.FilaDeEspera;
 import orders.OrdemServico;
 
 public class Main {
     public static void main(String[] args) {
-        
-        Cliente c1 = new Cliente();
-        FilaDeEspera f1 = new FilaDeEspera();
-        c1.setNome("Marcoooper");
-        c1.setHora("14:00");
-        c1.setCep(0);
-        Cliente c2= new Cliente();
-        c2.setNome("Kaio Bicha");
-        c2.setHora("15:00");
+
         Esteticista e1= new Esteticista();
         Procedimento pro1= new  Procedimento (); 
         Procedimento pro2= new  Procedimento ();
@@ -29,18 +20,5 @@ public class Main {
         oS.setProcedimento(pro1);
         double total= oS.total();  
         System.out.println("preço total: "+ total);
-        f1.setCliente(c1);
-        f1.setCliente(c1);
-        f1.setCliente(c1);
-        f1.setCliente(c1);
-        f1.setCliente(c1);
-        f1.setCliente(c1);
-        f1.setCliente(c1);
-        f1.setCliente(c1);
-        f1.setCliente(c1);
-        f1.setCliente(c2);
-        f1.setCliente(c1);
-        System.out.println(f1. getCliente(c1));
-        System.out.println(f1. getCliente(c2));
     }
 }
